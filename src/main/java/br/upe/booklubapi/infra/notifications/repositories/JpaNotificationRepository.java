7c3f673c6eebd6b4b4c2919316c38bb21424a721
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2026-06-03 09:48:24 -0300

    fixing merge details

--- src/main/java/br/upe/booklubapi/infra/notifications/repositories/JpaNotificationRepository.java
+++ src/main/java/br/upe/booklubapi/infra/notifications/repositories/JpaNotificationRepository.java
@@ -1,7 +1,5 @@
 package br.upe.booklubapi.infra.notifications.repositories;
 
-<<<<<<< HEAD
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,24 +8,5 @@ import br.upe.booklubapi.domain.notification.entities.Notification;
 import br.upe.booklubapi.domain.notification.repositories.NotificationRepository;
 
 public interface JpaNotificationRepository
-        extends NotificationRepository, JpaRepository<Notification, UUID> {
-
-    @Override
-    List<Notification> findByRecipientIdOrderByCreatedAtDesc(UUID recipientId);
-
-    @Override
-    long countByRecipientIdAndIsReadFalse(UUID recipientId);
+        extends JpaRepository<Notification, UUID>, NotificationRepository {
 }
-=======
-import br.upe.booklubapi.domain.notification.entities.Notification;
-import br.upe.booklubapi.domain.notification.repositories.NotificationRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
-
-@Repository
-public interface JpaNotificationRepository 
-    extends JpaRepository<Notification, UUID>, NotificationRepository {
-}
->>>>>>> 2cd68dc20da9c6724737186e3c454150291d3b88
